abdb4dd8945a03ce46bed2f9db4d82c2190ff681
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2025-06-08 18:39:18 -0300

    :tada: initial commit

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(300),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error(
+    "Oh, no!! As variáveis de ambiente nãop foram configuradas",
+    _env.error.format()
+  );
+
+  throw new Error("Oh, no!! As variáveis de ambiente nãop foram configuradas");
+}
+
+export const env = _env.data;
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import { app } from "./app";
+import { env } from "./env";
+
+app.listen({ host: "0.0.0.0", port: env.PORT }).then(() => {
+  console.log(`[API - ${env.NODE_ENV}] Tô rodandoooooooooo`);
+});
